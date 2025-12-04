09f9533a1669e40579e669811abd74f1d4bb640c
Stephanie Shi <shisteph@usc.edu>
2025-12-04 07:14:41 +0000

    hw5

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,7 +21,8 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
-
+bool scheduleHelper( const AvailabilityMatrix& avail, const size_t dailyNeed, const size_t maxShifts,
+    DailySchedule& sched, vector<int>& shifts, int day,int col);
 
 // Add your implementation of schedule() and other helper functions here
 
@@ -32,14 +33,48 @@ bool schedule(
     DailySchedule& sched
 )
 {
-    if(avail.size() == 0U){
+    if(avail.size() == 0){
         return false;
     }
     sched.clear();
     // Add your code below
+    
+    vector<Worker_T> tempRow(dailyNeed, INVALID_ID);
+    for (size_t i = 0; i < avail.size(); i++) {
+        sched.push_back(tempRow);
+    }
 
-
-
-
+    vector<int> shifts(avail[0].size(), 0);
+    return scheduleHelper(avail, dailyNeed, maxShifts, sched, shifts, 0, 0);
 }
 
+bool scheduleHelper(const AvailabilityMatrix& avail,const size_t dailyNeed, const size_t maxShifts, 
+    DailySchedule& sched, vector<int>& shifts, int day, int col){
+    if (day == (int)avail.size()){
+        return true;
+    }
+
+    if (col == (int)dailyNeed) {
+        return scheduleHelper(avail, dailyNeed, maxShifts, sched, shifts, day + 1, 0);
+    }
+
+    int worker = 0;
+    if (col > 0) {
+        worker = sched[day][col - 1] + 1;
+    }
+
+    for(int i = worker; i < (int)avail[0].size(); i++){
+        if (avail[day][i] && shifts[i] < (int)maxShifts) {
+            sched[day][col] = i;
+            shifts[i]++;
+
+            if (scheduleHelper(avail, dailyNeed, maxShifts, sched, shifts, day, col + 1)) {
+                return true;
+            }
+
+            shifts[i]--;
+            sched[day][col] = INVALID_ID;
+        }
+    }
+    return false;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,8 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void wordleHelper(const string& in, int fcount[26], const set<string>& dict,
+    string& curr, int pos, int ftotal, int free, set<string>& results);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +23,59 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
-
+  set<string> results;
+  string curr = in;
+  
+  int fcount[26] = {0};
+  int ftotal = 0;
+  for (int i = 0; i < (int)floating.length(); i++) {
+      fcount[floating[i] - 'a']++;
+      ftotal++;
+  }
+  
+  int free = 0;
+  for (int i = 0; i < (int)in.length(); i++) {
+      if (in[i] == '-') {
+          free++;
+      }
+  }
+  
+  wordleHelper(in, fcount, dict, curr, 0, ftotal, free, results);
+  return results;
 }
 
 // Define any helper functions here
+
+void wordleHelper(const string& in, int fcount[26], const set<string>& dict,
+    string& curr, int pos, int ftotal, int free, set<string>& results)
+{
+    if (pos == (int)in.length()) {
+        if (ftotal == 0 && dict.find(curr) != dict.end()) {
+            results.insert(curr);
+        }
+        return;
+    }
+
+    if (in[pos] != '-') {
+        curr[pos] = in[pos];
+        wordleHelper(in, fcount, dict, curr, pos + 1, ftotal, free, results);
+        return;
+    }
+    
+    if (ftotal > free) {
+        return;
+    }
+
+    for (char c = 'a'; c <= 'z'; c++) {
+        int idx = c - 'a';
+        if (fcount[idx] > 0) {
+            fcount[idx]--;
+            curr[pos] = c;
+            wordleHelper(in, fcount, dict, curr, pos + 1, ftotal - 1, free - 1, results);
+            fcount[idx]++;
+        } else if (free > ftotal) {
+            curr[pos] = c;
+            wordleHelper(in, fcount, dict, curr, pos + 1, ftotal, free - 1, results);
+        }
+    }
+}
